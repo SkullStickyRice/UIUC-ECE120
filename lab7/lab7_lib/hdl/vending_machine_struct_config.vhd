de9f84e5e110ddad175d4403cc83fe9e50685a35
SkullStickyRice <lxyestherli@gmail.com>
2017-05-17 15:48:51 -0500

    Add files via upload

---
+++ lab7/lab7_lib/hdl/vending_machine_struct_config.vhd
@@ -0,0 +1,11 @@
+-- Generation properties:
+--   Format              : hierarchical
+--   Generic mappings    : exclude
+--   Leaf-level entities : direct binding
+--   Regular libraries   : use library name
+--   View name           : include
+--   
+CONFIGURATION vending_machine_struct_config OF vending_machine IS
+   FOR struct
+   END FOR;
+END vending_machine_struct_config;
